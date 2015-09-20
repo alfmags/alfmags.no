bb22e2caa33a696c90b6a96ba09d9f1bd7f4215c
alfmags <alfmags@gmail.com>
2015-09-20 20:39:12 +0200

    in andre commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name alfmagsnoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the alfmagsnoApp
+ */
+angular.module('alfmagsnoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
