9cb6de9de1ade949a6e590a91a7e6f9583a344ac
unknown <asta@OSL-CL.sto.netlight.se>
2015-10-14 10:44:38 +0200

    startet på layout

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -1,11 +1,19 @@
 'use strict'
 
 angular.module('alfmagsnoApp')
-  .controller('BlogCtrl', function ($scope) {
+  .controller('BlogCtrl', function ($scope, $http) {
     $scope.test =  'HTML5 Boilerplate';
-   
 
+    $http.get('https://cors-test.appspot.com/test').then(function(resp)
+    {
 
+    	$scope.testRecieve = resp.data.status;
+    	console.log('Success', resp);
+    }, function(err){
+    	console.error('Err', err);
+    });
+
+    
       
    
   });
